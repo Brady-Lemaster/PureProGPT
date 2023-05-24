c77bac146dd69330359868c5f9fd0ed2f1efa95b
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-24 16:34:53 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,19 @@
 const CLIENT_ID = 'hWmT5ZuGe51IgiMD';
-
 const drone = new ScaleDrone(CLIENT_ID, {
   data: {
     name: 'PureProGPT',
     color: '#AFEDCB',
   },
 });
-
 function botRespond(prompt) {
   drone.publish({
     room: 'observable-room',
     message: prompt,
   });
 }
-
 drone.on('open', error => {
-  if (error) {
-    return console.error(error);
-  }
-  console.log('Successfully connected to Scaledrone');
-
   const room = drone.subscribe('observable-room');
-  
-  room.on('open', error => {
-    if (error) {
-      return console.error(error);
-    }
-    console.log('Successfully joined room');
-  });
-  
-  room.on('data', (text, member) => {
+  room.on('message', (text, member) => {
     if (member == 'PureProGPT') {}else{
       botRespond(text);
     }
