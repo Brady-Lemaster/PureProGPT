82c4bb89fab1fed563d1cd1f07ebd2b250a1260e
Brady-Lemaster <130093018+Brady-Lemaster@users.noreply.github.com>
2023-05-24 12:22:13 -0500

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,8 @@ function botRespond(msgToBot) {
   botSend(msgToBot)
 }
 
-room.on('data', (text, member) => {
-  botRespond(text)
-});
+while (2==2) {
+  room.on('data', (text, member) => {
+    botRespond(text)
+  });
+}
